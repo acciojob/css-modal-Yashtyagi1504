b40b37e1803be2572c9fcee78ab284548e279df0
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-04-06 03:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+.modal-content {
+  background-color: #fff;
+  margin: 15% auto;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.3);
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.close-modal {
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.modal-body {
+  margin-top: 10px;
+}
